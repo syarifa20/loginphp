fc00e3173efd33a3bf318af2e008323eafc526d7
syarifa20 <60133880+syarifa20@users.noreply.github.com>
2020-09-03 13:53:40 +0700

    validate.php

---
+++ validate.php
@@ -0,0 +1,27 @@
+<?php
+
+	$myemail = "syarifah@gmail.com";
+	$mypass  = "200403";
+
+	if (isset($_POST['login'])) {
+		$email = $_POST['email'];
+		$pass  = $_POST['password'];
+		
+		if( $email == $myemail and $pass == $mypass ){
+			if( isset($_POST['remember'])) {
+				setcookie('email', $email, time()+60*60*7);
+				setcookie('pass', $pass, time()+60*60*7);
+
+			}
+			session_start();
+			$_SESSION['email'] = $email;
+
+			header("location: welcome.php");
+
+		}else{
+			echo "Email or Password is Invalid.<br> click here to <a href='login.php'>try again</a>";
+		}
+	}else{
+		header("location: login.php");
+	}
+?>
